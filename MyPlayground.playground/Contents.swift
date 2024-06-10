6d9537382d22716cad6cd70017aaee2444d2bbc7
Irsyad Ashari <irsyad.ashari17@gmail.com>
2024-06-10 20:53:42 +0700

    move all files

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+func isPalindrome(text: String) -> Bool {
+    var leftPointer: Int = 0
+    var rightPointer: Int = text.count - 1
+    
+    let middleIndex = text.count / 2
+    
+    let arr: [Character = Array(text)
+              
+    while leftPointer < middleIndex {
+        if arr[leftPointer] != arr[rightPointer] {
+            return false
+        }
+        leftPointer += 1
+        rightPointer -= 1
+    }
+              
+    return true
+}
+
+let text = "gagag"
+print(isPalindrome(text: text))
